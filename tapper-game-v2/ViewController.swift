972b8c7fbda92bd41a4c728839059ce756e99d1b
xXx_MLG_sweg_420_quikscoper_lel_69_LeGiT_haXor_xXx <liam.nickell25@gmail.com>
2016-06-25 00:02:37 -0500

    Initial Commit

---
+++ tapper-game-v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapper-game-v2
+//
+//  Created by Liam Nickell on 6/25/16.
+//  Copyright © 2016 Liam Nickell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
